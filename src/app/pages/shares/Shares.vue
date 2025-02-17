a92be9c798b2c934d82b979d9bc6d08de672a923
Marcelo Moreli <marcelo.moreli@hotmail.com>
2025-02-16 19:30:02 -0600

    adding 2 pages

---
+++ src/app/pages/shares/Shares.vue
@@ -0,0 +1,12 @@
+<template>
+  <BudgetPane type="shares">
+    <template #title>
+      <YearToggle key-path="page.shares.sharesFor" />
+    </template>
+  </BudgetPane>
+</template>
+
+<script lang="ts" setup>
+import BudgetPane from '@components/feature/BudgetPane.vue';
+import YearToggle from '@components/feature/YearToggle.vue';
+</script>
